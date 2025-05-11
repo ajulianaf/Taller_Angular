f0dc720fd2cd142a60adc469865be5e6d0605353
Karen Barreto <kjbarreto@uniboyaca.edu.co>
2025-05-11 16:52:59 -0500

    contacto

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,10 +2,12 @@ import { Routes } from '@angular/router';
 import { HomeComponent } from './components/home/home.component';
 import { ColeccionesComponent } from './components/colecciones/colecciones.component';
 import { PromocionesComponent } from './components/promociones/promociones.component';
+import { ContactoComponent } from './components/contacto/contacto.component'; // <--- Importación agregada
 
 export const routes: Routes = [
     { path: '', component: HomeComponent },
     { path: 'colecciones', component: ColeccionesComponent },
     { path: 'promociones', component: PromocionesComponent },
+    { path: 'contacto', component: ContactoComponent }, // <--- Ruta agregada
     { path: '**', redirectTo: '' }
 ];
